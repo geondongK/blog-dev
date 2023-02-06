f0b3e0f3512ba2b86c079f9c1b86d752cc457371
GeonDong <ehd563@naver.com>
2023-02-06 14:15:37 +0900

    Feat 로그인 시 navbar 상태관리 추가

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,67 +1,55 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable no-console */
-//   eslint-disable
+/* eslint-disable */
 import React, { useEffect, useRef, useState } from 'react';
-// useNavigate
-import { Link } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faMagnifyingGlass,
     faBars,
     faRightToBracket,
-    // faRightFromBracket,
+    faRightFromBracket,
     faUserPlus,
     faXmark,
 } from '@fortawesome/free-solid-svg-icons';
-// import customAxios from '../libs/api/axios';
-// import { persistor } from '../redux/store/store';
+import customAxios from '../libs/api/axios';
+import { persistor } from '../redux/store/store';
 
 function NavBar() {
     const checkedRef = useRef(false);
     const [search, setSearch] = useState('');
     const [checked, setChecked] = useState(true);
 
-    console.log(search, setSearch);
+    // console.log(search, setSearch);
 
-    // const navigate = useNavigate();
+    const navigate = useNavigate();
 
     // 사용자 로그인 상태여부 체크.
-    // const { currentUser } = useSelector(state => state.user);
-
-    // 메뉴 오픈.
-    // const [anchorEl, setAnchorEl] = useState(null);
-
-    // const open = Boolean(anchorEl);
-    // const handleClick = e => {
-    //     setAnchorEl(e.currentTarget);
-    // };
-    // const handleClose = () => {
-    //     setAnchorEl(null);
-    // };
+    const { currentUser } = useSelector(state => state.user);
 
     // persistor 세션 값삭 제.
-    // const purge = async () => {
-    //     await persistor.purge();
-    // };
+    const purge = async () => {
+        await persistor.purge();
+    };
 
-    // const onKeyPress = e => {
-    //     if (e.key === 'Enter') {
-    //         alert('hi');
-    //     }
-    // };
+    const onKeyDown = e => {
+        if (e.key === 'Enter') {
+            alert('hi');
+        }
+    };
 
-    // const handleLogout = () => {
-    //     customAxios
-    //         .post('/auth/logout', {})
-    //         .then(() => {
-    //             navigate('/');
-    //             // header 입력
-    //             // dispatch(logout({}))
-    //             // window.location.reload();
-    //         })
-    //         .catch(() => {});
-    // };
+    const handleLogout = () => {
+        customAxios
+            .post('/auth/logout', {})
+            .then(() => {
+                navigate('/');
+                // header 입력
+                // dispatch(logout({}))
+                // window.location.reload();
+            })
+            .catch(() => {});
+    };
 
     const handleChecked = () => {
         setChecked(!checked);
@@ -100,36 +88,58 @@ function NavBar() {
                         <FontAwesomeIcon icon={faMagnifyingGlass} />
                     </i>
                     <input
+                        onKeyDown={onKeyDown}
                         className="search-input"
                         type="search"
                         placeholder="검색"
                     />
                 </div>
             </div>
-            <div className="nav-right">
-                <Link to="/login">
-                    <button onClick={handleChecked} type="button">
+            {!currentUser ? (
+                <div className="nav-right">
+                    <Link to="/login">
+                        <button onClick={handleChecked} type="button">
+                            <i>
+                                <FontAwesomeIcon
+                                    className="right-icon"
+                                    icon={faRightToBracket}
+                                />
+                            </i>
+                            로그인
+                        </button>
+                    </Link>
+                    <Link to="/register">
+                        <button onClick={handleChecked} type="button">
+                            <i>
+                                <FontAwesomeIcon
+                                    className="right-icon"
+                                    icon={faUserPlus}
+                                />
+                            </i>
+                            회원가입
+                        </button>
+                    </Link>
+                </div>
+            ) : (
+                <div className="nav-right">
+                    <button
+                        onClick={() => {
+                            handleLogout();
+                            purge();
+                            handleChecked();
+                        }}
+                        type="button"
+                    >
                         <i>
                             <FontAwesomeIcon
                                 className="right-icon"
-                                icon={faRightToBracket}
+                                icon={faRightFromBracket}
                             />
                         </i>
-                        로그인
+                        로그아웃
                     </button>
-                </Link>
-                <Link to="/register">
-                    <button onClick={handleChecked} type="button">
-                        <i>
-                            <FontAwesomeIcon
-                                className="right-icon"
-                                icon={faUserPlus}
-                            />
-                        </i>
-                        회원가입
-                    </button>
-                </Link>
-            </div>
+                </div>
+            )}
             {!checkedRef.current.checked ? (
                 <label htmlFor="nav-menu">
                     <FontAwesomeIcon icon={faBars} />
